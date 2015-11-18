2901940867e12d02bf52d5d95bd72e40552f8620
Tres Seaver <tseaver@palladion.com>
2015-11-18 13:48:14 -0500

    Export names at top level.

--- pyramid_nacl_session/__init__.py
+++ pyramid_nacl_session/__init__.py
@@ -1,1 +1,10 @@
-# package
+def _export(thing):
+    """ No-op; document that ``thing`` is an API.
+
+    Also, shuts pyflakes up about unused import.
+    """
+
+from .serializer import EncryptingPickleSerializer
+_export(EncryptingPickleSerializer)
+from .scripts import generate_secret
+_export(generate_secret)
