be992fa5b194f2a173310c747a82492b20993b8c
Michael Merickel <michael@merickel.org>
2015-11-24 09:50:24 -0600

    test the secret box

--- pyramid_nacl_session/tests/test_serializer.py
+++ pyramid_nacl_session/tests/test_serializer.py
@@ -10,67 +10,40 @@ class EncryptedSerializerTests(unittest.TestCase):
     def _makeOne(self, *args, **kw):
         return self._getTargetClass()(*args, **kw)
 
+    def _serialize(self, value, secret, nonce):
+        import base64
+        from pyramid.compat import pickle
+        from nacl.secret import SecretBox
+
+        cstruct = pickle.dumps(value, pickle.HIGHEST_PROTOCOL)
+        fstruct = SecretBox(secret).encrypt(cstruct, nonce)
+        return base64.urlsafe_b64encode(fstruct).rstrip(b'=')
+
     def test_ctor_w_invalid_key(self):
-        SECRET = 'SEEKRIT!'
+        SECRET = b'SEEKRIT!'
         self.assertRaises(ValueError, self._makeOne, SECRET)
 
     def test_dumps(self):
-        from pyramid.compat import pickle
         from .. import serializer as MUT
-        SECRET = 'SEEKRIT!' * 4  # 32 bytes
+        SECRET = b'SEEKRIT!' * 4  # 32 bytes
         NONCE = b'\x01' * 24
         APPSTRUCT = {'foo': 'bar'}
-        PICKLED = pickle.dumps(APPSTRUCT, pickle.HIGHEST_PROTOCOL)
-        _base64_called = []
-        def _base64_encode(what):
-            _base64_called.append(what)
-            return what
-        with _Monkey(MUT,
-                     SecretBox=_SecretBox,
-                     random=lambda size: NONCE,
-                     urlsafe_b64encode=_base64_encode):
-            eps = self._makeOne(SECRET)
+        expected = self._serialize(APPSTRUCT, SECRET, NONCE)
+
+        eps = self._makeOne(SECRET)
+        with _Monkey(MUT, random=lambda size: NONCE):
             encrypted = eps.dumps(APPSTRUCT)
-            pickled, nonce = encrypted[:-25], encrypted[-24:]
-            self.assertEqual(pickled, PICKLED)
-            self.assertEqual(nonce, NONCE)
-            self.assertEqual(_base64_called, [encrypted])
+        self.assertEqual(expected, encrypted)
 
     def test_loads(self):
-        from pyramid.compat import pickle
-        from .. import serializer as MUT
-        SECRET = 'SEEKRIT!' * 4  # 32 bytes
+        SECRET = b'SEEKRIT!' * 4  # 32 bytes
         NONCE = b'\x01' * 24
         APPSTRUCT = {'foo': 'bar'}
-        PICKLED = pickle.dumps(APPSTRUCT, pickle.HIGHEST_PROTOCOL)
-        CIPHERTEXT = PICKLED + b':' + NONCE
-        _base64_called = []
-        def _base64_decode(what):
-            _base64_called.append(what)
-            return what
-        with _Monkey(MUT, SecretBox=_SecretBox,
-                     urlsafe_b64decode=_base64_decode):
-            eps = self._makeOne(SECRET)
-            loaded = eps.loads(CIPHERTEXT)
-            self.assertEqual(loaded, APPSTRUCT)
-            self.assertEqual(_base64_called, [CIPHERTEXT + b'=='])
+        CIPHERTEXT = self._serialize(APPSTRUCT, SECRET, NONCE)
 
-
-class _SecretBox(object):
-
-    KEY_SIZE = 32
-    NONCE_SIZE = 24
-
-    def __init__(self, key):
-        assert len(key) == self.KEY_SIZE
-        self._key = key
-
-    def encrypt(self, plaintext, nonce):
-        assert len(nonce) == self.NONCE_SIZE
-        return plaintext + b':' + nonce
-
-    def decrypt(self, ciphertext):
-        return ciphertext[:-(self.NONCE_SIZE + 1)]
+        eps = self._makeOne(SECRET)
+        loaded = eps.loads(CIPHERTEXT)
+        self.assertEqual(loaded, APPSTRUCT)
 
 
 class _Monkey(object):
