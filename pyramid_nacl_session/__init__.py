5fd7241372210c43f4dcf81bf4b1441e6663e123
Julien Cigar <julien@perdition.city>
2019-12-17 17:05:05 +0100

    add __all__ to appease flake8

--- pyramid_nacl_session/__init__.py
+++ pyramid_nacl_session/__init__.py
@@ -6,6 +6,9 @@ from .session import EncryptedCookieSessionFactory
 from .scripts import generate_secret
 from .utils import session_factory_from_settings
 
+__all__ = ['EncryptedSerializer', 'EncryptedCookieSessionFactory',
+           'generate_secret', 'session_factory_from_settings']
+
 
 def includeme(config):
     session_factory = session_factory_from_settings(config.registry.settings)
